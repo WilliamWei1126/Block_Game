2474cc215b044261dfc74552f24880cb1b323f4b
WilliamWei1126 <67813037+WilliamWei1126@users.noreply.github.com>
2026-05-04 21:07:53 -0500

    Variable height

--- Final_Project/design_source/mb_block/sim/mb_block.v
+++ Final_Project/design_source/mb_block/sim/mb_block.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
-//Date        : Mon May  4 19:57:53 2026
+//Date        : Mon May  4 20:22:09 2026
 //Host        : Will-PC-9600X running 64-bit major release  (build 9200)
 //Command     : generate_target mb_block.bd
 //Design      : mb_block
